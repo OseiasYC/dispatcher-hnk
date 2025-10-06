6eaa12d1456f05bec4a9b723ae1ed2b0507143e0
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2025-10-06 16:41:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form/Form";
-// import Closed from "./components/Closed/ClosedPage";
+import Closed from "./components/Closed/ClosedPage";
 
 export function App() {
-  // const [horaAtual, setHoraAtual] = useState(new Date().getHours());
+  const [horaAtual, setHoraAtual] = useState(new Date().getHours());
 
-  // useEffect(() => {
-  //   const intervalo = setInterval(() => {
-  //     setHoraAtual(new Date().getHours());
-  //   }, 1000 * 60);
-  //   return () => clearInterval(intervalo);
-  // }, []);
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setHoraAtual(new Date().getHours());
+    }, 1000 * 60);
+    return () => clearInterval(intervalo);
+  }, []);
   
   return (
-  <div>
-    <Form />
-    {/* {horaAtual >= 17 ? <Closed /> : <Form />} */}
-  </div>
-);
-
+    <div>
+      {horaAtual >= 17 ? <Closed /> : <Form />}
+    </div>
+  );
 }
 
 export default App;

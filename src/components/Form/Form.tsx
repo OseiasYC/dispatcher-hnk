2c976edd53a83fc56ae9b2fa31244b5466e74ec7
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2025-12-26 14:47:49 -0300

    Update symbols for form actions

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -53,9 +53,9 @@ const formatarMensagem = (data: {
   id: string;
 }) => {
   const simbolosTipo: Record<string, string> = {
-    alterar: "🎄 ALTERAR",
-    digitar: "🎁 DIGITAR",
-    cancelar: "🪨 CANCELAR",
+    alterar: "🍾🥂 ALTERAR",
+    digitar: "🎆 DIGITAR",
+    cancelar: "🍺 CANCELAR",
   };
 
   const titulo = `*${simbolosTipo[data.tipo]}* - \`${data.id}\``;
